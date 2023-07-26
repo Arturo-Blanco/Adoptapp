892f8728e10e0e441e693e349391c1d0ca962bed
ValeriaSanabria <valeriasanabria@tdf.edu.ar>
2023-07-26 06:44:13 -0300

    formularioAdoptarOk

--- adopt-app/src/Components/Formularios/Input.jsx
+++ adopt-app/src/Components/Formularios/Input.jsx
@@ -3,7 +3,7 @@ import './Formularios.css';
 const ComponenteInput = (props) =>{
     return(
         <div>
-            <label className="form-label" htmlFor={props.id}>{props.label}</label>
+            <label className="form-label-input" htmlFor={props.id}>{props.label}</label>
             <div className="grupoInput">
                 <input className="form-input" type={props.type} placeholder={props.placeholder} id={props.id} name={props.name} value={props.value} onChange={props.onChange} required
                 />
--- adopt-app/src/Components/Formularios/Terminos.jsx
+++ adopt-app/src/Components/Formularios/Terminos.jsx
@@ -4,7 +4,7 @@ const ComponenteTerminos = (props) =>{
 
     return(
         <div className="contenedorTerminos">
-            <label className='form-label' htmlFor={props.id}>
+            <label className='form-label-terminos' htmlFor={props.id}>
             <input className='form-input' type="checkbox" name={props.id} value={props.value} id={props.id} onChange={props.onChange} required/>
             {props.label}
             </label>
