523e20ab0be8358c7865bdad08e23224141cb310
ValeriaSanabria <valeriasanabria@tdf.edu.ar>
2023-07-27 12:27:24 -0300

    linkRequisitos

--- adopt-app/src/Components/Adopciones/adopciones.jsx
+++ adopt-app/src/Components/Adopciones/adopciones.jsx
@@ -76,7 +76,16 @@ const Adopciones = () => {
     return (
         
         <div className='adopciones'>
-            <h2>Un amigo te esta esperando</h2>
+            <article className='container-title'>
+                <div className='div-container-title'>
+                <h3 className='text-requisito'>Un amigo te esta esperando</h3>
+                <a href="/informaciongeneral">
+                    <div className='link-requisitos'>
+                    <h4>Ver requisitos</h4>
+                    </div>
+                </a>
+                </div>
+            </article>
             <Filter filter={filter} setFilters={setFilters}></Filter>
             <CardAdopt pageNumber={pageNumber} filters={values}/>
             <div className='pagination-container'>
@@ -105,31 +114,6 @@ const Adopciones = () => {
                 onClick={nextPage}
                 text="Siguiente >"
             />
-            </div>
-            <div className='row'>
-                <div className='col-1'>
-                </div>
-                <div className='col-2'>
-                    <h3 className='titulo-requisito'>Requisitos para adoptar</h3>
-                    <div className='requisitos'>
-                        <span className='count-punto'>1
-                        </span>
-                        <p className='texto-req'>Ser mayor de 18 años</p>
-                    </div>
-                    <div className='requisitos'>
-                        <span className='count-punto'>2
-                        </span>
-                        <p className='texto-req'>Amar a las mascotas y poder dedicarle el tiempo que necesite.</p>
-                    </div><div className='requisitos'>
-                        <span className='count-punto'>3
-                        </span>
-                        <p className='texto-req'>Querer sumar un integrante a tu vida por el resto de la suya, sin importar los cambios que se presenten.</p>
-                    </div><div className='requisitos'>
-                        <span className='count-punto'>4
-                        </span>
-                        <p className='texto-req'>Comprometerse con el cuidado, la salud y la castración de la mascota.</p>
-                    </div>
-                </div>
             </div>
         </div>
     )
--- adopt-app/src/Components/Info-Gral/infoGral.jsx
+++ adopt-app/src/Components/Info-Gral/infoGral.jsx
@@ -2,10 +2,35 @@ import './infoGral.css';
 
 const InfoGral = () => {
     return(
-       <div>
+        <div>
         <h2>Acá va la página de Información General</h2>
+        <div className='row'>
+                <div className='col-1'>
+                </div>
+                <div className='col-2'>
+                    <h3 className='titulo-requisito'>Requisitos para adoptar</h3>
+                    <div className='requisitos'>
+                        <span className='count-punto'>1
+                        </span>
+                        <p className='texto-req'>Ser mayor de 18 años</p>
+                    </div>
+                    <div className='requisitos'>
+                        <span className='count-punto'>2
+                        </span>
+                        <p className='texto-req'>Amar a las mascotas y poder dedicarle el tiempo que necesite.</p>
+                    </div><div className='requisitos'>
+                        <span className='count-punto'>3
+                        </span>
+                        <p className='texto-req'>Querer sumar un integrante a tu vida por el resto de la suya, sin importar los cambios que se presenten.</p>
+                    </div><div className='requisitos'>
+                        <span className='count-punto'>4
+                        </span>
+                        <p className='texto-req'>Comprometerse con el cuidado, la salud y la castración de la mascota.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-       </div>
     )
 };
 export default InfoGral;
